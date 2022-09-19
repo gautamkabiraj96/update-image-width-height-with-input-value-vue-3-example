d52041e91515e47087e09d08f65c2b48aa12a2a1
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2022-09-20 01:59:42 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# update-image-width-height-with-input-value-vue-3-example
-Created with CodeSandbox
+# Vue 3 update image width and height from input values with example
+
+Learn how to change height and width of an img element with input values in Vue.js with demo example.
+[Learn more](https://www.nightprogrammer.com/vue-js/how-to-update-src-url-of-an-image-with-a-button-click-in-vue-3-example/)
---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h3>Vue 3 change image width and height with input values</h3>
+    <input type="number" v-model="imageWidth" inputmode="numeric" />
+    <input type="number" v-model="imageHeight" inputmode="numeric" />
+    <div class="np-img-container">
+      <img
+        src="https://picsum.photos/id/1/500/400"
+        :style="{
+          height: `${imageHeight}px`,
+          width: `${imageWidth}px`,
+        }"
+      />
+    </div>
+    <!--
+      www.nightprogrammer.com
+    -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      imageWidth: 500,
+      imageHeight: 400,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.np-img-container {
+  margin: 20px 0;
+}
+</style>
